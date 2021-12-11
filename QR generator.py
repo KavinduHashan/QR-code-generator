dbb5929fbfa66c649710fc73ee9670a086fd7034
Kavindu Hashan <89184906+KavinduHashan@users.noreply.github.com>
2021-12-11 21:29:15 +0530

    Add files via upload

---
+++ QR generator.py
@@ -0,0 +1,18 @@
+# install qrcode
+# install image
+
+import qrcode
+import image
+
+qr = qrcode.QRCode(
+    version=15,
+    box_size=10,  # size of qrcode box
+    border=5,
+)
+
+data = "https://www.youtube.com/"  # url of youtube
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill="black", back_color="white")
+img.save("test.png")
